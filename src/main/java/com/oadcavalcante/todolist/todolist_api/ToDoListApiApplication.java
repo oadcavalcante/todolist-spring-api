b961e706b48127ccc37ea0eee563978a6c967533
3S Cavalcante <aderc19@gmail.com>
2024-12-07 20:47:07 -0300

    :tada: first commit.

---
+++ src/main/java/com/oadcavalcante/todolist/todolist_api/ToDoListApiApplication.java
@@ -0,0 +1,13 @@
+package com.oadcavalcante.todolist.todolist_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListApiApplication.class, args);
+	}
+
+}
