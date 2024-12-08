639446d5dd6ee509546c05a9561fd9ea708988c6
3S Cavalcante <aderc19@gmail.com>
2024-12-07 21:55:39 -0300

    :sparkles: feat: Criação do controller TodoController.

---
+++ src/main/java/com/oadcavalcante/todolist/todolist_api/controller/TodoController.java
@@ -0,0 +1,35 @@
+package com.oadcavalcante.todolist.todolist_api.controller;
+
+import com.oadcavalcante.todolist.todolist_api.entity.Todo;
+import com.oadcavalcante.todolist.todolist_api.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    @Autowired
+    private TodoService todoService;
+
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo){
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list(){
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo){
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable Long id){
+        return todoService.delete(id);
+    }
+}
