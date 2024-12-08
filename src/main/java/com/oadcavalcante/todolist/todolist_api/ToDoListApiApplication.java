eea931a50ef4810e1c71ff5f2f6f11b58d8e4b52
3S Cavalcante <aderc19@gmail.com>
2024-12-07 22:08:12 -0300

    :sparkles: feat: Configurações da conexão com o banco de dados PostgreSQL.

--- src/main/java/com/oadcavalcante/todolist/todolist_api/ToDoListApiApplication.java
+++ src/main/java/com/oadcavalcante/todolist/todolist_api/ToDoListApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ToDoListApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApiApplication.class, args);
 	}
-
 }
